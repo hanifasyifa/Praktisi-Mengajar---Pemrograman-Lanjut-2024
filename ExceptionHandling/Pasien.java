6ad103507c4af7ff7fd29f59959b0cf0559cb4c4
hanifasyifa <160991315+hanifasyifa@users.noreply.github.com>
2024-05-15 12:15:01 +0700

    Create Pasien.java

---
+++ ExceptionHandling/Pasien.java
@@ -0,0 +1,18 @@
+public class Pasien {
+        private String nama;
+        private String nomorAntrean;
+    
+        public Pasien(String nama, String nomorAntrean) {
+            this.nama = nama;
+            this.nomorAntrean = nomorAntrean;
+        }
+    
+        public String getNama() {
+            return nama;
+        }
+    
+        public String getAntrean() {
+            return nomorAntrean;
+        }
+    }
+    
